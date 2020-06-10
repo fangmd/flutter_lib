3a353cd8af40af616ce519777bf491d41f616858
fangmingdong <fangmingdong0224@gmail.com>
2020-06-10 17:31:53 +0800

    [p_utils] log change

--- p_utils/lib/src/logger.dart
+++ p_utils/lib/src/logger.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/material.dart';
 import 'package:meta/meta.dart';
 import 'common_function.dart';
 
 class Logger {
   static void d({tag: '', @required Object msg}) {
     if (isDebug()) {
-      print('ChiCha:$tag ==> $msg');
+      debugPrint('ChiCha:$tag ==> $msg');
     }
   }
 }
