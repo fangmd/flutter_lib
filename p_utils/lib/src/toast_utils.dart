c731132b0c40eea9628c283c048a79fba126eb1b
Zakerberg <blmichael1129@icloud.com>
2020-10-14 11:49:36 +0800

    [Fix] toast颜色

--- p_utils/lib/src/toast_utils.dart
+++ p_utils/lib/src/toast_utils.dart
@@ -8,6 +8,8 @@ class ToastUtils {
       toastLength: Toast.LENGTH_SHORT,
       gravity: ToastGravity.CENTER,
       timeInSecForIosWeb: 1,
+      backgroundColor: Colors.grey,
+      textColor: Colors.white,
       fontSize: 16.0,
     );
   }
